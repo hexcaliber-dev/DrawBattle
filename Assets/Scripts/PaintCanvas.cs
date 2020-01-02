c30edc19dcb07b112377281383c274bb47ca001c
Ben Cuan <github@bencuan.me>
2020-01-02 14:04:35 -0800

    Major drawing code cleanup

--- Assets/Scripts/PaintCanvas.cs
+++ Assets/Scripts/PaintCanvas.cs
@@ -1,28 +1,87 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-public class PaintCanvas : MonoBehaviour
-{
-    public Texture2D Texture { get; private set; }
+public class PaintCanvas : MonoBehaviour {
+    public Texture2D texture { get; private set; }
 
-    public byte[] GetAllTextureData()
-    {
-        return Texture.GetRawTextureData();
+    /// Fineness of angle sweeping when drawing circles. Smaller = more precise circles; larger = faster
+    public float angleStep = 0.1f;
+
+    // How many seconds in between canvas updates
+    public float updateInterval = 1 / 60f;
+
+    /// Flattened array of pixels and their colors. See https://docs.unity3d.com/ScriptReference/Texture2D.SetPixels32.html for more info
+    private Color32[] cur_colors;
+
+    public byte[] GetAllTextureData() {
+        return texture.GetRawTextureData();
     }
 
-    private void Start()
-    {
+    private void Start() {
         PrepareTemporaryTexture();
+        StartCoroutine(UpdateCanvas(updateInterval));
     }
 
-    private void PrepareTemporaryTexture()
-    {
-        Texture = (Texture2D)GameObject.Instantiate(GetComponent<Renderer>().material.mainTexture);
-        GetComponent<Renderer>().material.mainTexture = Texture;
+    private void PrepareTemporaryTexture() {
+        texture = (Texture2D) GameObject.Instantiate(GetComponent<Renderer>().material.mainTexture);
+        GetComponent<Renderer>().material.mainTexture = texture;
     }
 
-    internal void SetAllTextureData(byte[] textureData)
-    {
-        Texture.LoadRawTextureData(textureData);
-        Texture.Apply();
+    internal void SetAllTextureData(byte[] textureData) {
+        texture.LoadRawTextureData(textureData);
+        texture.Apply();
+    }
+
+    /// Creates a single dot at a location given a color and brush size
+    public void BrushAreaWithColor(Vector2 pixelUV, Color color, int size) {
+        // Polar coords are cool and good (plus they are ok at drawing circles i guess)
+        for (int r = 0; r < size; r++) {
+            for (float angle = 0; angle < Mathf.PI * 2; angle += angleStep) {
+                float x1 = r * Mathf.Cos(angle);
+                float y1 = r * Mathf.Sin(angle);
+
+                // Need to transform x and y coordinates to flat coordinates of array
+                int array_pos = (int) (texture.width * (int) (pixelUV.y + y1) + (pixelUV.x + x1));
+
+                // Check if this is a valid position
+                if (array_pos > cur_colors.Length || array_pos < 0)
+                    return;
+
+                // Update pixel array
+                cur_colors[array_pos] = color;
+            }
+        }
+    }
+
+    /// Lerps between two points, filling in the gaps with color. Adapted from https://assetstore.unity.com/packages/tools/painting/free-draw-simple-drawing-on-sprites-2d-textures-113131#content
+    public void ColorBetween(Vector2 start_point, Vector2 end_point, Color color, int width) {
+        // Get the distance from start to finish
+        float distance = Vector2.Distance(start_point, end_point);
+        Vector2 direction = (start_point - end_point).normalized;
+
+        Vector2 cur_position = start_point;
+
+        // Calculate how many times we should interpolate between start_point and end_point based on the amount of time that has passed since the last update
+        float lerp_steps = 1 / distance * 10;
+
+        for (float lerp = 0; lerp <= 1; lerp += lerp_steps) {
+            cur_position = Vector2.Lerp(start_point, end_point, lerp);
+            BrushAreaWithColor(cur_position, color, width);
+        }
+    }
+
+    /// Applies changes to canvas. Smaller updateInterval = faster updates, but makes the game laggier
+    private IEnumerator UpdateCanvas(float updateInterval) {
+        while (true) {
+            if (texture != null) {
+                if (cur_colors != null) {
+                    texture.SetPixels32(cur_colors);
+                    texture.Apply();
+                }
+                cur_colors = texture.GetPixels32();
+            }
+            yield return new WaitForSeconds(updateInterval);
+        }
     }
 }
--- Assets/Scripts/PlayerDraw.cs
+++ Assets/Scripts/PlayerDraw.cs
@@ -8,25 +8,13 @@ public class PlayerDraw : PlayerDrawBehavior {
 
     public PaintCanvas paintCanvas;
 
-    /// Fineness of angle sweeping when drawing circles. Smaller = more precise circles; larger = faster
-    public float angleStep = 0.1f;
-
-    // How many seconds in between canvas updates
-    public float updateInterval = 0.25f;
-
+    /// Last frame's mouse position. Used for lerping
     Vector2 prevPos;
+
+    /// Used to keep track of mouseup/down events so we don't fill in the space between 2 points if there was a pen lift
     bool isDragging = false;
 
-    Color32[] cur_colors;
-
-    private void Start() {
-
-        StartCoroutine(UpdateCanvas(updateInterval));
-        var data = paintCanvas.GetAllTextureData();
-        var zippeddata = data.Compress();
-
-        // RpcSendFullTexture(zippeddata);
-    }
+    private void Start() { }
 
     private void Update() {
 
@@ -40,9 +28,8 @@ public class PlayerDraw : PlayerDrawBehavior {
             if (Physics.Raycast(ray, out hit)) {
                 var pallet = hit.collider.GetComponent<PaintCanvas>();
                 if (pallet != null) {
-                    // Debug.Log(hit.textureCoord);
-                    // Debug.Log(hit.point);
 
+                    // Check to make sure the canvas was clicked
                     Renderer rend = hit.transform.GetComponent<Renderer>();
                     MeshCollider meshCollider = hit.collider as MeshCollider;
 
@@ -51,26 +38,16 @@ public class PlayerDraw : PlayerDrawBehavior {
 
                     Texture2D tex = rend.material.mainTexture as Texture2D;
                     Vector2 pixelUV = hit.textureCoord;
+
                     pixelUV.x *= tex.width;
                     pixelUV.y *= tex.height;
-
-                    // CmdBrushAreaWithColorOnServer(pixelUV, ColorPicker.SelectedColor, BrushSizeSlider.BrushSize);
-                    // BrushAreaWithColor(pixelUV, ColorPicker.SelectedColor, BrushSizeSlider.BrushSize);
-
-                    // if (firstMarkMade && Vector2.Distance(prevPos, pixelUV) > LERP_THRESHOLD) {
-                    //     for (
-                    //         float lerpVal = LERP_STEP; lerpVal < 1f; lerpVal += LERP_STEP
-                    //     ) {
-                    //         BrushAreaWithColor(prevPos * (1 - lerpVal) + pixelUV * lerpVal, Color.black, 10);
-                    //     }
-                    // }
 
                     if (!isDragging)
                         prevPos = pixelUV;
                     else
-                        ColorBetween(prevPos, pixelUV, Color.black, 10);
+                        paintCanvas.ColorBetween(prevPos, pixelUV, Color.black, 10);
 
-                    BrushAreaWithColor(pixelUV, Color.black, 10);
+                    paintCanvas.BrushAreaWithColor(pixelUV, Color.black, 10);
                     prevPos = pixelUV;
                     isDragging = true;
                 }
@@ -78,73 +55,8 @@ public class PlayerDraw : PlayerDrawBehavior {
         }
     }
 
-    public void ColorBetween(Vector2 start_point, Vector2 end_point, Color color, int width) {
-        // Get the distance from start to finish
-        float distance = Vector2.Distance(start_point, end_point);
-        Vector2 direction = (start_point - end_point).normalized;
-
-        Vector2 cur_position = start_point;
-
-        // Calculate how many times we should interpolate between start_point and end_point based on the amount of time that has passed since the last update
-        float lerp_steps = 1 / distance * 10;
-
-        for (float lerp = 0; lerp <= 1; lerp += lerp_steps) {
-            cur_position = Vector2.Lerp(start_point, end_point, lerp);
-            BrushAreaWithColor(cur_position, color, width);
-        }
-    }
-
-    // [Command]
-    // private void CmdBrushAreaWithColorOnServer(Vector2 pixelUV, Color color, int size) {
-    //     RpcBrushAreaWithColorOnClients(pixelUV, color, size);
-    //     BrushAreaWithColor(pixelUV, color, size);
-    // }
-
-    // [ClientRpc]
-    // private void RpcBrushAreaWithColorOnClients(Vector2 pixelUV, Color color, int size) {
-    //     BrushAreaWithColor(pixelUV, color, size);
-    // }
-
     public override void SendFullTexture(RpcArgs args) {
         byte[] textureData = args.GetNext<byte[]>();
         paintCanvas.SetAllTextureData(textureData.Decompress());
-    }
-
-    private void BrushAreaWithColor(Vector2 pixelUV, Color color, int size) {
-        // for (int x = -size; x < size; x++) {
-        //     for (int y = -size; y < size; y++) {
-        //         paintCanvas.Texture.SetPixel((int) pixelUV.x + x, (int) pixelUV.y + y, color);
-        //     }
-        // }
-        for (int r = 0; r < size; r++) {
-            for (float angle = 0; angle < Mathf.PI * 2; angle += angleStep) {
-                float x1 = r * Mathf.Cos(angle);
-                float y1 = r * Mathf.Sin(angle);
-
-                // Need to transform x and y coordinates to flat coordinates of array
-                int array_pos = (int) (paintCanvas.Texture.width * (int) (pixelUV.y + y1) + (pixelUV.x + x1));
-
-                // Check if this is a valid position
-                if (array_pos > cur_colors.Length || array_pos < 0)
-                    return;
-
-                cur_colors[array_pos] = color;
-                // ((int) (pixelUV.x + x1), (int) (pixelUV.y + y1), color);
-            }
-        }
-
-    }
-
-    private IEnumerator UpdateCanvas(float updateInterval) {
-        while (true) {
-            if (paintCanvas.Texture != null) {
-                if(cur_colors != null) {
-                    paintCanvas.Texture.SetPixels32(cur_colors);
-                    paintCanvas.Texture.Apply();
-                }
-                cur_colors = paintCanvas.Texture.GetPixels32();
-            }
-            yield return new WaitForSeconds(updateInterval);
-        }
     }
 }
