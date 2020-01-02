a38d5a48f69fb4e95194a5e86745aaf8097fa01c
Epicrider <omarepic1@gmail.com>
2020-01-02 14:15:36 -0800

    Add Movement PlayerController
    Requires Network touchup for different player to move according to
    their own keyboard.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    private int translate;
+    [SerializeField] private float speed;
+    private float translate;
+    private Vector3 changeVector;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,8 +23,11 @@ public class PlayerController : MonoBehaviour
 
     void VerticalMovement()
     {
-        if ( Input.GetKey(KeyCode.W) ) {translate = 1;}
-        if ( Input.GetKey(KeyCode.S) ) {translate = -1;}
+        if ( Input.GetKey(KeyCode.W) ) {translate = 1f * speed;}
+        else if ( Input.GetKey(KeyCode.S) ) {translate = -1f * speed;}
+        else {translate = 0;}
+        changeVector = new Vector2(0, translate);
+        transform.position += changeVector;
     }
 
     void AimMovement()
