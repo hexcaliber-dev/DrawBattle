82d34a7314f34335ba2d3ccca04fca0d86cc0756
Epicrider <omarepic1@gmail.com>
2020-01-02 13:48:51 -0800

    Provide Further Organization

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    private int translate;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,18 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        VerticalMovement();
+        AimMovement();
+    }
+
+    void VerticalMovement()
+    {
+        if ( Input.GetKey(KeyCode.W) ) {translate = 1;}
+        if ( Input.GetKey(KeyCode.S) ) {translate = -1;}
+    }
+
+    void AimMovement()
+    {
+
     }
 }
